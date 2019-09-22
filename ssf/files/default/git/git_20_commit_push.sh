03d5a3b28b4f306913af7f690dbb05989e7ab3b5
Imran Iqbal <iqbalmy@hotmail.com>
2019-09-22 06:55:05 +0100

    feat: allow pushing directly to upstream remote
    BREAKING CHANGE: Multiple arguments have been added to the `commit_push`
    shell script.  This includes logically reordering the arguments.
    
    BREAKING CHANGE: The pillar structure has been modified according to the
    changes made.

--- ssf/files/default/git/git_20_commit_push.sh
+++ ssf/files/default/git/git_20_commit_push.sh
@@ -4,12 +4,18 @@
 
 # Collect incoming arguments
 STATE=${1}
-BRANCH_PR=${2}
-COMMIT_GREP=${3}
-COMMIT_TITLE=${4}
-COMMIT_BODY=${5}
-COMMIT_OPTIONS=${6}
-PUSH_TO_REMOTE=${7}
+BRANCH_UPSTREAM=${2}
+BRANCH_PR=${3}
+COMMIT_GREP=${4}
+COMMIT_TITLE=${5}
+COMMIT_BODY=${6}
+COMMIT_OPTIONS=${7}
+PUSH_ACTIVE=${8}
+PUSH_VIA_PR=${9}
+REMOTE_FORK_NAME=${10}
+REMOTE_FORK_BRANCH=${11}
+REMOTE_UPSTREAM_NAME=${12}
+REMOTE_UPSTREAM_BRANCH=${13}
 # Prepare initial state line variables
 CHANGED=True
 COMMENT='Command `'${STATE}'` run'
@@ -26,7 +32,16 @@ fi
 
 # Perform actions
 git commit ${AMEND} "${COMMIT_OPTIONS}" -m "${COMMIT_TITLE}" -m "${COMMIT_BODY}"
-git push ${FORCE} -u ${PUSH_TO_REMOTE} ${BRANCH_PR}
+if [ "${PUSH_ACTIVE}" = "True" ]; then
+    if [ "${PUSH_VIA_PR}" = "True" ]; then
+        git push ${FORCE} -u ${REMOTE_FORK_NAME} ${BRANCH_PR}
+    else
+        git checkout ${BRANCH_UPSTREAM}
+        git merge ${BRANCH_PR}
+        git push ${REMOTE_UPSTREAM_NAME} HEAD:${REMOTE_UPSTREAM_BRANCH}
+        git branch -d ${BRANCH_PR}
+    fi
+fi
 
 # Write the state line
 echo "changed=${CHANGED} comment='${COMMENT}'"
